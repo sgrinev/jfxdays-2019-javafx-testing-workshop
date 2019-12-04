9e2ae245ded0e0e783093cfa0ad5856847f7787a
Sergey Grinev <sergeygrinev@mail.ru>
2019-12-04 02:03:21 +0100

    screenshot utility and test task

---
+++ ArtShopApp/test/jfxdays/testing/artshopapp/ScreenshotTest.java
@@ -0,0 +1,63 @@
+package jfxdays.testing.artshopapp;
+
+import javafx.application.Platform;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import jfxdays.testing.util.ScreenshotsSupport;
+import org.junit.After;
+import org.junit.Test;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit.ApplicationTest;
+import org.testfx.matcher.base.ColorMatchers;
+import org.testfx.util.WaitForAsyncUtils;
+
+import javax.imageio.ImageIO;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.matcher.control.LabeledMatchers.hasText;
+import static org.testfx.util.DebugUtils.*;
+
+public class ScreenshotTest extends ApplicationTest {
+    Scene scene;
+
+    public void start (Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("landing.fxml"));
+        Parent root = loader.load();
+        stage.setTitle("Art Shop");
+        stage.setScene(scene = new Scene(root, 400, 500));
+        stage.show();
+    }
+
+    @Test
+    public void testBlue() throws IOException {
+        clickOn("#tfSearch");
+        write("green");
+
+        Label node = lookup("#labelCount").query();
+
+        // TASK: setup golden folder
+        // TASK: test image in the table instead
+        ScreenshotsSupport.assertSnapshotsEqual("testBlue", node, 0.05);
+    }
+
+    @After
+    public void tearDown () throws Exception {
+        FxToolkit.cleanupStages();
+        release(new KeyCode[]{});
+        release(new MouseButton[]{});
+    }
+}
---
+++ ArtShopApp/test/jfxdays/testing/util/ScreenshotsSupport.java
@@ -0,0 +1,80 @@
+package jfxdays.testing.util;
+
+import javafx.application.Platform;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.geometry.Bounds;
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.WritableImage;
+import org.junit.Assert;
+import org.testfx.util.WaitForAsyncUtils;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+import java.util.stream.IntStream;
+
+public class ScreenshotsSupport {
+    public static void saveScreenshot(WritableImage img, String name) throws IOException {
+        ImageIO.write(SwingFXUtils.fromFXImage(img, null), "png", new File(name + ".png"    ));
+    }
+
+    public static void saveScreenshot(Node node, String name) throws IOException {
+        saveScreenshot(getImage(node), null);
+    }
+
+    public static double screenshotDistance(final Image image1, final Image image2) {
+        final int width = (int) image1.getWidth();
+        final int height = (int) image1.getHeight();
+        // TODO: validate image2 size
+        final PixelReader reader1 = image1.getPixelReader();
+        final PixelReader reader2 = image2.getPixelReader();
+
+        final double sum = IntStream.range(0, width).parallel().
+                mapToDouble(i -> {
+                    return IntStream.range(0, height).parallel().mapToDouble(j -> {
+                        double res = 0;
+                        int argb1 = reader1.getArgb(i, j);
+                        int argb2 = reader2.getArgb(i, j);
+                        if (argb1 == argb2) {
+                            return 0;
+                        } else {
+                            // ignoring opacity for now
+                            for (int k = 0; k < 3; k++) {
+                                int d = argb1 & 0xFF - argb2 & 0xFF;
+                                res += d * d / 65536d;
+                                argb1 >>= 8;
+                                argb2 >>= 8;
+                            }
+                            System.out.println(Math.sqrt(res));
+                            return Math.sqrt(res);
+                        }
+                    }).sum();
+                }).sum();
+
+        return sum / (width * height);
+    }
+
+    private static WritableImage getImage(Node node) {
+        Bounds bounds = node.getBoundsInLocal();
+        final WritableImage img = new WritableImage((int) bounds.getWidth(), (int) bounds.getHeight());
+        Platform.runLater(() -> node.snapshot(null, img));
+        WaitForAsyncUtils.waitForFxEvents();
+        return img;
+    }
+
+    public static void assertSnapshotsEqual(final String name, final Node nodeUnderTest, final double threshold) throws IOException {
+        final WritableImage image = getImage(nodeUnderTest);
+        final Image golden = new Image("file:golden/" + name + ".png");
+        if (golden.isError()) {
+            saveScreenshot(image, name);
+            Assert.fail( "Golden image doesn't exist for " + name);
+        }
+        double dist = screenshotDistance(golden, getImage(nodeUnderTest));
+        if (dist > threshold) {
+            saveScreenshot(image, name);
+            Assert.fail( "The two snapshots differ with distance " + dist);
+        }
+    }
+}
