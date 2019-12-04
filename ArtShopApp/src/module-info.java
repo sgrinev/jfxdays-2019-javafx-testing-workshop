fc7ef5642fafab86f73aa231aa21702b92eb839b
BacLuc <lucius.bachmann@gmx.ch>
2019-12-04 09:33:03 +0100

    build: setup for maven

---
+++ ArtShopApp/src/module-info.java
@@ -0,0 +1,8 @@
+module jfxdays.testing.artshop {
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.swing;
+    opens jfxdays.testing.artshopapp;
+}
